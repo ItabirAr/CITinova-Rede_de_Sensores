30ffc93ef9aae0976dfdfe74787cffb8a91d6519
JDaiky <>
2023-02-08 23:21:21 -0300

    novas pastas para cada sensor
    Neste commit foram criadas uma pasta para cada sensor utilizado no projeto

---
+++ MQ135/README.md
@@ -0,0 +1,13 @@
+# Sensor DHT11
+![myimage-alt-tag](https://bnz05pap001files.storage.live.com/y4mU3eoX_tDy79Aye5wkD-PeYiAYDzDxGjAuiWKzoV6usCktN_Tq8bUfw5kLTJ_6gsDOXUhSv5_4CSCasRsr184-u8UAsyz8P7DxLmZyylT1ZGgFJX4c4a54ZMncGwUro3R0L0hIrfctP1vp4n2wWJdSS_OBxB22392geMIgy0WU4kf6INAwcoYu3WLqrFsqwgrKPcg0QAkJ-q__D5mwwMVWQ?encodeFailures=1&width=799&height=577)
+
+# Descrição do Sensor
+Sensor simples, mede temperatura, pressão
+
+<h1> Informações Técnicas </h1>
+	
+<ul>
+	<li>leitura analogico</li>
+	<li>Faixa de leitura 30°C- 90°C</li>
+	<li>...</li>
+<ul>
---
+++ UVM30A/sensor.ino
@@ -0,0 +1,35 @@
+#include <DHT.h> //biblioteca do sensor DHT11
+
+#define pinoDHT A3 //pino  usado para a conexão
+#define modeloDHT DHT11 //modelo do sensor
+DHT dht(pinoDHT, modeloDHT); // define dht como objeto do tipo DHT
+float Temperatura; //vetor para os dados lidos de temperatura
+float Umidade; //vetor para os dados lidos de umidade
+
+void setup()
+{
+  Serial.begin(9600); //inicialização do Monitor Serial
+  dht.begin(); //inicialização o sensor DHT11
+}
+
+void loop()
+{
+  //Atribuição dos valores medidos
+  Temperatura = dht.readTemperature(); //temperatura informada pelo sensor
+  Umidade = dht.readHumidity(); //umidade informada pelo sensor
+
+  //Impressão dos dados no Monitor Serial
+  if (isnan(Temperatura) || isnan(Umidade)) { //se for atribuido um valor não numérico as variáveis, indica erro de leitura no sensor
+    Serial.println("Falha de leitura do DHT11."); 
+  } 
+  else { //caso contrário, imprime os valores
+    Serial.print("Temperatura: "); 
+    Serial.print(Temperatura);
+    Serial.print(" °C"); //temperatura em Graus Celsius
+    Serial.print("\t"); //espaçamentos entre os dados
+    Serial.print("Umidade Relativa do Ar: ");
+    Serial.print(Umidade);
+    Serial.println(" %UR"); //percentual de Umidade Relativa do Ar
+  }
+  delay(1000); //intervalo de 1 segundo
+}
