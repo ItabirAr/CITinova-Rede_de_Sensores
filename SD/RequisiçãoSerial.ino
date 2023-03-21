c62d25f66ecd51f6f00d1866ff7e4e78932c14e0
Geraldo Carvalho <82607547+geraldoneto7@users.noreply.github.com>
2023-03-21 15:25:34 -0300

    Update RequisiçãoSerial.ino

--- SD/RequisiçãoSerial.ino
+++ SD/RequisiçãoSerial.ino
@@ -3,6 +3,7 @@
 
 // Cria uma variável para representar o arquivo de dados e define o pino do chip select do cartão SD
 File dataFile;
+char valueSerial;
 const int chipSelect = 53;
 
 void setup() {
@@ -20,43 +21,49 @@ void setup() {
   Serial.println("Programa iniciado.");
 }
 
-void requisicaoSerial(){
-  // Lê o tempo inicial e final digitado pelo usuário
-    unsigned long start_time = Serial.parseInt();
-    unsigned long end_time = Serial.parseInt();
-    Serial.print("Tempo inicial: ");
-    Serial.println(start_time);
-    Serial.print("Tempo final: ");
-    Serial.println(end_time);
-
-    // Abre o arquivo de dados
-    dataFile = SD.open("BMP.txt");
-    if (dataFile) {
-      // Lê os dados do arquivo e envia pela porta serial
-      while (dataFile.available()) {
-        String request = dataFile.readStringUntil('\n');
-        String timeStr = request.substring(0, request.indexOf(','));
-        unsigned long time = timeStr.toInt();
-        if (time >= start_time && time <= end_time) {
-          String valueStr = request.substring(request.indexOf(',') + 1);
-          float value = valueStr.toFloat();
-          Serial.print("Tempo: ");
-          Serial.print(time);
-          Serial.print(" | Valor: ");
-          Serial.println(value);
-        }
-      }
-      // Fecha o arquivo de dados
-      dataFile.close();
-    } else {
-      // Mensagem de erro caso não seja possível abrir o arquivo de dados
-      Serial.println("Erro ao abrir arquivo de dados.");
+void requisicaoGetData(){
+  dataFile = SD.open("BMP.txt");
+  if(dataFile){
+    while(dataFile.available()){
+      Serial.write(dataFile.read());
     }
+  }
+}
+
+void requisicaoGetInterval(){
+  unsigned long start_time = Serial.parseInt();
+  unsigned long end_time = Serial.parseInt();
+  Serial.print("Tempo inicial: ");
+  Serial.println(start_time);
+  Serial.print("Tempo final: ");
+  Serial.println(end_time);
+
+  dataFile = SD.open("BMP.txt");
+  if(dataFile){
+    while(dataFile.available()){
+      String request = dataFile.readStringUntil('\n');
+      String timeStr = request.substring(0, request.indexOf(','));
+      unsigned long time = timeStr.toInt();
+      if (time >= start_time && time <= end_time) {
+        String valueStr = request.substring(request.indexOf(',') + 1);
+        Serial.print("Tempo: ");
+        Serial.print(time);
+        Serial.print(" | Valor: ");
+        Serial.println(valueStr);
+      }
+    }
+  }
 }
 
 void loop() {
   // Verifica se há dados disponíveis na porta serial
   if (Serial.available()) {
-    requisicaoSerial();
+    valueSerial = Serial.read();
+    if(valueSerial == '1'){
+      requisicaoGetData();
+   }
+    if(valueSerial == '2'){
+      requisicaoGetInterval();
+    }
   }
 }
